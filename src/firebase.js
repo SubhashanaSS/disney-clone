538c18cf77ae0c72df242ad44d22550c4b3cef49
SubhashanaSS <subhashana201@gmail.com>
2022-06-21 13:40:17 +0530

    firebase api

---
+++ src/firebase.js
@@ -0,0 +1,32 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCUXOw5Ue0HriFxVDfH7ib1gN95H988Z4",
+    authDomain: "disneyplus-clone-4dd4e.firebaseapp.com",
+    projectId: "disneyplus-clone-4dd4e",
+    storageBucket: "disneyplus-clone-4dd4e.appspot.com",
+    messagingSenderId: "927362412083",
+    appId: "1:927362412083:web:792f4c6802332242bf3d46"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export {auth, provider, storage};
+export default db;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

63edf7023912b88827f84b43ab1ddf8a80a60ddc
SubhashanaSS <subhashana201@gmail.com>
2022-06-27 12:50:10 +0530

    firebase authentication success

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import 'firebase/compat/storage';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCCUXOw5Ue0HriFxVDfH7ib1gN95H988Z4",
     authDomain: "disneyplus-clone-4dd4e.firebaseapp.com",
@@ -7,14 +12,14 @@ const firebaseConfig = {
     appId: "1:927362412083:web:792f4c6802332242bf3d46"
   };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
-const provider = new firebase.auth.GoogleAuthProvider();
-const storage = firebase.storage();
-
-export {auth, provider, storage};
-export default db;
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const storage = firebase.storage().ref();
+  
+  export { auth, provider, storage };
+  export default db;
 
 
 

34ae177f4e1faac618da852e353085d798104a4c
Subhashana Samaranayake <subhashana201@gmail.com>
2022-07-08 00:23:27 +0530

    Delete firebase key

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/firestore";
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCCUXOw5Ue0HriFxVDfH7ib1gN95H988Z4",
+    apiKey: "",
     authDomain: "disneyplus-clone-4dd4e.firebaseapp.com",
     projectId: "disneyplus-clone-4dd4e",
     storageBucket: "disneyplus-clone-4dd4e.appspot.com",
-    messagingSenderId: "927362412083",
-    appId: "1:927362412083:web:792f4c6802332242bf3d46"
+    messagingSenderId: "",
+    appId: ""
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
